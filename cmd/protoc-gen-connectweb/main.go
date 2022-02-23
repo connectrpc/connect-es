32f6a88801c9892861c9e3c4bac14b8ab0f7a573
Timo Stamm <ts@timostamm.de>
2022-02-23 17:20:26 +0100

    Add Go connectweb generator
    The connectweb generator generates type information for connect-web.

---
+++ cmd/protoc-gen-connectweb/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"github.com/bufconnect/connect-web/internal/connectweb_generator"
+	"github.com/bufconnect/connect-web/internal/protoplugin"
+	"google.golang.org/protobuf/types/pluginpb"
+)
+
+func main() {
+	protoplugin.Options{
+		Name:    "protoc-gen-connectweb",
+		Version: version,
+	}.Pipe(func(gen *protoplugin.Generator) error {
+		gen.SupportedFeatures = uint64(pluginpb.CodeGeneratorResponse_FEATURE_PROTO3_OPTIONAL)
+		for _, file := range gen.Files {
+			if !file.Generate {
+				continue
+			}
+			connectweb_generator.GenerateFile(gen, file)
+		}
+		return nil
+	})
+}
