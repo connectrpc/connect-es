bd3464d3e2428a84d0003b6f73f43a3d21ceafe4
Timo Stamm <ts@timostamm.de>
2022-03-01 20:18:39 +0100

    Update code-size benchmark for most recent connect-web version
    Shaving off 179 bytes with the more capable version 🎯

--- packages/bench-codesize/report.md
+++ packages/bench-codesize/report.md
@@ -9,5 +9,5 @@ minify the bundle, and compress it like a web server would usually do.
 
 | code generator                         | bundle size        | minified               | gzip               |
 |----------------------------------------|-------------------:|-----------------------:|-------------------:|
-| [connect-web](src/entry-connectweb.ts) | 269,267 b | 142,809 b | 25,575 b |
+| [connect-web](src/entry-connectweb.ts) | 269,500 b | 142,474 b | 25,396 b |
 | [grpc-web](src/entry-grpcweb.ts)       | 1,019,177 b    | 724,458 b    | 90,848 b    |
--- packages/bench-codesize/src/entry-connectweb.ts
+++ packages/bench-codesize/src/entry-connectweb.ts
@@ -1,9 +1,15 @@
-import { makePromiseClient } from "@bufbuild/connect-web";
+import {
+  makePromiseClient,
+  createConnectTransport,
+} from "@bufbuild/connect-web";
 import { PluginService } from "./gen/connectweb/buf/alpha/registry/v1alpha1/plugin_connectweb.js";
 
-const pluginClient = makePromiseClient(PluginService, {
-  baseUrl: "https://localhost",
-});
+const pluginClient = makePromiseClient(
+  PluginService,
+  createConnectTransport({
+    baseUrl: "https://localhost",
+  })
+);
 
 // eslint-disable-next-line no-console -- log statement makes sure the variable is in use
 console.log(pluginClient);
