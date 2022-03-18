e5fccf2e16e987f214bb7d94fb0a84bc741bd53f
Timo Stamm <ts@timostamm.de>
2022-03-19 00:03:54 +0100

    make format produced a diff?

--- packages/connect-web/src/promise-client.ts
+++ packages/connect-web/src/promise-client.ts
@@ -12,10 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import type {
-  MethodInfoServerStreaming,
-  MethodInfoUnary,
-  PartialMessage,
+import type {   MethodInfoServerStreaming,   MethodInfoUnary, PartialMessage,
   ServiceType,
 } from "@bufbuild/protobuf";
 import { MethodKind } from "@bufbuild/protobuf";
