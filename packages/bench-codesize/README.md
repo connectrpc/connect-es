e75b33da3f0562f7e7a161e92322c794c36bb2e7
Timo Stamm <ts@timostamm.de>
2022-03-19 02:45:01 +0100

    Make code size benchmark prettier

---
+++ packages/bench-codesize/README.md
@@ -0,0 +1,13 @@
+# Code size comparison
+
+This is a simple code size comparison between Connect-Web and gRPC-web.
+
+We are generating code for the module [buf.build/bufbuild/buf](https://buf.build/bufbuild/buf)
+once with `protoc-gen-grpc-web`, once with `protoc-gen-connect-web`. Then we bundle Then we bundle [a client](./src) 
+for the service `buf.alpha.registry.v1alpha1.PluginService` with [esbuild](https://esbuild.github.io/),
+minify the bundle, and compress it like a web server would usually do.
+
+| code generator | bundle size        | minified               | gzip                 |
+|----------------|-------------------:|-----------------------:|---------------------:|
+| connect-web    | 273,316 b | 142,437 b | 20,584 b |
+| grpc-web       | 1,019,177 b    | 724,458 b    | 74,076 b    |
--- packages/bench-codesize/report.mjs
+++ packages/bench-codesize/report.mjs
@@ -10,14 +10,14 @@ process.stdout.write(`# Code size comparison
 This is a simple code size comparison between Connect-Web and gRPC-web.
 
 We are generating code for the module [buf.build/bufbuild/buf](https://buf.build/bufbuild/buf)
-once with \`protoc-gen-grpc-web\`, once with \`protoc-gen-connect-web\`. Then we bundle a client 
+once with \`protoc-gen-grpc-web\`, once with \`protoc-gen-connect-web\`. Then we bundle Then we bundle [a client](./src) 
 for the service \`buf.alpha.registry.v1alpha1.PluginService\` with [esbuild](https://esbuild.github.io/),
 minify the bundle, and compress it like a web server would usually do.
 
-| code generator                         | bundle size        | minified               | gzip               |
-|----------------------------------------|-------------------:|-----------------------:|-------------------:|
-| [connect-web](src/entry-connectweb.ts) | ${connectweb.size} | ${connectweb.minified} | ${connectweb.brotli} |
-| [grpc-web](src/entry-grpcweb.ts)       | ${grpcweb.size}    | ${grpcweb.minified}    | ${grpcweb.brotli}    |
+| code generator | bundle size        | minified               | gzip                 |
+|----------------|-------------------:|-----------------------:|---------------------:|
+| connect-web    | ${connectweb.size} | ${connectweb.minified} | ${connectweb.brotli} |
+| grpc-web       | ${grpcweb.size}    | ${grpcweb.minified}    | ${grpcweb.brotli}    |
 `);
 
 
