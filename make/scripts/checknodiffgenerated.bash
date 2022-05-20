0c4828f8353f27f130cd0b306721e5894f611e11
Timo Stamm <ts@timostamm.de>
2022-05-20 18:18:15 +0200

    Fix trap in checknodiffgenerated.bash

--- make/scripts/checknodiffgenerated.bash
+++ make/scripts/checknodiffgenerated.bash
@@ -10,9 +10,7 @@ fail() {
 STATUS_SHORT_PRE_FILE="$(mktemp)"
 STATUS_SHORT_POST_FILE="$(mktemp)"
 STATUS_SHORT_DIFF_FILE="$(mktemp)"
-trap 'rm -rf "${STATUS_SHORT_PRE_FILE}"' EXIT
-trap 'rm -rf "${STATUS_SHORT_POST_FILE}"' EXIT
-trap 'rm -rf "${STATUS_SHORT_DIFF_FILE}"' EXIT
+trap 'rm -rf "${STATUS_SHORT_PRE_FILE}" "${STATUS_SHORT_POST_FILE}" "${STATUS_SHORT_DIFF_FILE}"' EXIT
 
 git status --short > "${STATUS_SHORT_PRE_FILE}"
 "$@"
