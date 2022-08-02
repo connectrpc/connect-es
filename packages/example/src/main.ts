e4faa7fe73820cb1c56ae17754909db46e554a22
Timo Stamm <ts@timostamm.de>
2022-08-02 19:55:55 +0200

    Format example (#216)

--- packages/example/src/main.ts
+++ packages/example/src/main.ts
@@ -1,18 +1,34 @@
-import { createConnectTransport, createPromiseClient } from "@bufbuild/connect-web";
+// Copyright 2021-2022 Buf Technologies, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import {
+  createConnectTransport,
+  createPromiseClient,
+} from "@bufbuild/connect-web";
 import { ElizaService } from "./eliza_connectweb";
 
-const root = document.querySelector<HTMLElement>("#root")
+const root = document.querySelector<HTMLElement>("#root");
 const input = document.createElement("input");
 
-void (async() => {
-
+void (async () => {
   const transport = createConnectTransport({
-    baseUrl: "https://demo.connect.build"
+    baseUrl: "https://demo.connect.build",
   });
 
   const client = createPromiseClient(ElizaService, transport);
 
-  print("What is your name?")
+  print("What is your name?");
   const name = await prompt();
   print(`> ${name}`);
 
@@ -26,13 +42,12 @@ void (async() => {
     const res = await client.say({ sentence });
     print(res.sentence);
   }
-
 })();
 
 function print(text: string): void {
   const p = document.createElement("p");
   p.innerText = text;
-  p.scrollIntoView()
+  p.scrollIntoView();
   root?.append(p);
 }
 
@@ -40,8 +55,8 @@ function prompt(): Promise<string> {
   input.value = "";
   root?.append(input);
   input.focus();
-  return new Promise<string>(resolve => {
-    input.onkeyup = ev => {
+  return new Promise<string>((resolve) => {
+    input.onkeyup = (ev) => {
       if (ev.key == "Enter" && input.value.length > 0) {
         input.remove();
         input.onkeyup = null;
