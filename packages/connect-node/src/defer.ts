6af3154d9174ce945d3e8102a2c4d47e89d19280
Timo Stamm <ts@timostamm.de>
2022-11-08 19:17:25 +0100

    Explain the catch() in defer.ts

--- packages/connect-node/src/defer.ts
+++ packages/connect-node/src/defer.ts
@@ -6,7 +6,11 @@ export function defer<T>(): Promise<T> & Ctrl<T> {
     rej = reject;
   });
   void p.catch(() => {
-    //
+    // We want to provide several promises that are typically awaited
+    // by the user one after the other.
+    // If we reject one of the promises before it is awaited by the user,
+    // some runtimes may report an unhandled promise rejection.
+    // We are attaching this error handler to avoid the warnings.
   });
   const c: Ctrl<T> = {
     resolve(v) {
