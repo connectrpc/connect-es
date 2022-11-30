433c03fc1019985dd3d1748808cdd7a208459935
Timo Stamm <ts@timostamm.de>
2022-11-30 19:30:33 +0100

    Add more logs

--- packages/connect-node/src/private/connect-error-from-node.ts
+++ packages/connect-node/src/private/connect-error-from-node.ts
@@ -40,36 +40,44 @@ function isSyscallError(
   // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-explicit-any
   const r = reason as any;
   if (reason instanceof Error) {
-    if ("code" in r) {
-      // TODO
-      // eslint-disable-next-line no-console
-      console.error(r);
-      // eslint-disable-next-line no-console
-      console.error("want syscall", syscall, "want code", code);
-      // eslint-disable-next-line no-console
-      console.log("syscall in r", "syscall" in r)
-      if ("syscall" in r) {
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access,no-console
-        console.log("r.syscall", r.syscall)
-      }
-      // eslint-disable-next-line no-console
-      console.log("code in r", "code" in r)
-      if ("code" in r) {
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access,no-console
-        console.log("r.code", r.code)
-      }
-      // eslint-disable-next-line no-console
-      console.log("syscall = " + syscall, "syscall" in r)
-    }
+    // if ("code" in r) {
+    //   // TODO
+    //   // eslint-disable-next-line no-console
+    //   console.error(r);
+    //   // eslint-disable-next-line no-console
+    //   console.error("want syscall", syscall, "want code", code);
+    //   // eslint-disable-next-line no-console
+    //   console.log("syscall in r", "syscall" in r)
+    //   if ("syscall" in r) {
+    //     // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access,no-console
+    //     console.log("r.syscall", r.syscall)
+    //   }
+    //   // eslint-disable-next-line no-console
+    //   console.log("code in r", "code" in r)
+    //   if ("code" in r) {
+    //     // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access,no-console
+    //     console.log("r.code", r.code)
+    //   }
+    //   // eslint-disable-next-line no-console
+    //   console.log("syscall = " + syscall, "syscall" in r)
+    // }
     if ("code" in r && "syscall" in r) {
       // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-unsafe-member-access
       if (syscall === r.syscall && code === r.code) {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-explicit-any,@typescript-eslint/no-unsafe-member-access,no-console
+        console.log(`isSyscallError ${syscall} ${code} OK`, reason)
         return true;
+      } else {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-explicit-any,@typescript-eslint/no-unsafe-member-access,no-console
+        console.log(`isSyscallError ${syscall} ${code} NO`, reason)
       }
     }
     if ("cause" in reason) {
       // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-explicit-any,@typescript-eslint/no-unsafe-member-access
       return isSyscallError(r.cause, syscall, code);
+    } else {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-explicit-any,@typescript-eslint/no-unsafe-member-access,no-console
+      console.log(`isSyscallError ${syscall} ${code} NO, and cause also NO`, reason)
     }
   }
   return false;
