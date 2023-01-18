af6f70f235ed61cd5d16aa668911b475e80a86f8
Timo Stamm <ts@timostamm.de>
2023-01-18 20:28:03 +0100

    Add compressionNegotiate() to @bufbuild/connect-core (#423)

--- packages/connect-core/src/compression.ts
+++ packages/connect-core/src/compression.ts
@@ -12,6 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+import { ConnectError } from "./connect-error.js";
+import { Code } from "./code.js";
+
 /**
  * compressedFlag indicates that the data in a EnvelopedMessage is
  * compressed. It has the same meaning in the gRPC-Web, gRPC-HTTP2,
@@ -41,4 +44,60 @@ export interface Compression {
    * size exceeds readMaxBytes.
    */
   decompress: (bytes: Uint8Array, readMaxBytes: number) => Promise<Uint8Array>;
+}
+
+/**
+ * Validates the request encoding and determines the accepted response encoding.
+ *
+ * Returns the request and response compression to use. If the client requested
+ * an encoding that is not available, the returned object contains an error that
+ * must be used for the response.
+ */
+export function compressionNegotiate(
+  available: Compression[],
+  requested: string | null, // e.g. the value of the Grpc-Encoding header
+  accepted: string | null, // e.g. the value of the Grpc-Accept-Encoding header
+  headerNameAcceptEncoding: string // e.g. the name of the Grpc-Accept-Encoding header
+): {
+  request: Compression | null;
+  response: Compression | null;
+  error?: ConnectError;
+} {
+  let request = null;
+  let response = null;
+  let error = undefined;
+  if (requested !== null && requested !== "identity") {
+    const found = available.find((c) => c.name === requested);
+    if (found) {
+      request = found;
+    } else {
+      // To comply with https://github.com/grpc/grpc/blob/master/doc/compression.md
+      // and the Connect protocol, we return code "unimplemented" and specify
+      // acceptable compression(s).
+      const acceptable = available.map((c) => c.name).join(",");
+      error = new ConnectError(
+        `unknown compression "${requested}": supported encodings are ${acceptable}`,
+        Code.Unimplemented,
+        {
+          [headerNameAcceptEncoding]: acceptable,
+        }
+      );
+    }
+  }
+  if (accepted === null || accepted === "") {
+    // Support asymmetric compression. This logic follows
+    // https://github.com/grpc/grpc/blob/master/doc/compression.md and common
+    // sense.
+    response = request;
+  } else {
+    const acceptNames = accepted.split(",").map((n) => n.trim());
+    for (const name of acceptNames) {
+      const found = available.find((c) => c.name === name);
+      if (found) {
+        response = found;
+        break;
+      }
+    }
+  }
+  return { request, response, error };
 }
