4f5fac391cce52eac5d6c8c7acafbf449b73ca9b
Timo Stamm <ts@timostamm.de>
2023-02-03 16:43:57 +0100

    Update the self-signed certificated we use in tests (#450)

--- packages/connect-node-test/src/helpers/testserver.ts
+++ packages/connect-node-test/src/helpers/testserver.ts
@@ -15,6 +15,8 @@
 import * as http2 from "http2";
 import * as http from "http";
 import * as https from "https";
+import * as fs from "fs";
+import * as path from "path";
 import {
   compressionGzip,
   createConnectTransport,
@@ -34,56 +36,7 @@ export function createTestServers() {
   let nodeHttpServer: http.Server | undefined;
   let nodeHttpsServer: http.Server | undefined;
 
-  // openssl req -x509 -newkey rsa:2048 -nodes -sha256 -subj '/CN=localhost' -keyout localhost-privkey.pem -out localhost-cert.pem
-  const certLocalhost = {
-    key: `-----BEGIN PRIVATE KEY-----
-MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDDmkdOoHdbvOsh
-zJ87KpSu4I3KOPuO/2msLNYDmiyivGc79Y5T064sCxet0J/jfpU/7t9A4ETNGBi0
-/i4ZGa/kbR4uCQXnl2jroHuNFdfcnyBWjJLxaIN2WUm/RZvsjNSoWhkOjNjZZIsg
-9nGv6iJLNVH7RZf0bAmklo+Al6bXnsPY+9k8Apy74zniGBchJsU5q8Mg4eX8ctVp
-faWLg0LUCOZGQZoJPc1ZsxcMGOBN+7CIONq/VeiNB8ILyadF6A4Jywr9FkJzCz0k
-r5uiXlYygsXSsMYTcScPy3Uqd3W9YKwhVaE8fsLa56+Gp08ifRnyUC9E0y56BpOQ
-SEGis/J5AgMBAAECggEAYhMNBeN3dUv4KW5LuILVIgn9jhOcLkrqVSX1+MBo+P+2
-U14ACw/sGLFHIiOkx5BEjVXcIs9QL/WRDc8UlglHJCWCKFrl1wUX2m0RBeCZ50jy
-EfNSCjD5GXIThBAG6kiCqBbk0WLVp8q9570pq6ado1AM1wxB8I/rFddYBIYFZb6X
-l3kr9ncGuRY3X3swVJvxsBu1FWVPKBbesHDeuJpUV8vdQges8rrVj+5iAB0yo0YS
-HMOFfwDImfrO+GltfGenAcbQo+zfDJutsZmcE+1+pQNAXHeHIQ1GNX1xKSlg/39L
-hlIVVNm8U2uYsrGR/eSHGT7ith8q2iyr3oRXq5oGAQKBgQD4mDnFYVnQBIij7Q88
-saVtRetX69aCGdFxEAiLE95NM6GfmX6ecW/lbWxqN31PWn8x9Th6NkwkjrJgmOZ0
-EEwiv/A2ABNCBXTJJVnkjU4eHA4IpWA9B9kd17mm1vypesLzqtPFcUXZC5/j4m8r
-mUsb1gzlJSanjOL5E0CtmUfBaQKBgQDJbfAT61bVQP3HvdtjfWbObn1o/G9K+by7
-JHQU4LdWhoF5vEX3ffDuKkI8KJjNbxJAKjBxd9GVKnbXf5lEjXp8fqFBiYL+fAae
-xghLX2ekG80a6oDHLcapL5Cd4Rfu/+UXHz1dt/idq7GHaK5lKxiWY0RJfhzkN2ct
-1MAbCIF2kQKBgQC8zHfLtQTN3DOeM9NHqC9YDysOqhlO06biNGgYoC1L+rN8D8o/
-MS3nU7Rr8tqjorpJSdDr2EL53LoSbO1RJnN8TMN1b5cwpA4vGcPstXinm2z5Ggg+
-MbCuXoEr+ckIOijHgGy3XjDLB24WV4lQ1mpZqLZAbXeu9mwG+J09etUpSQKBgBnR
-TYRAFdEfk2eWnhgSaqrq3ogmUt15RQd3lKlYZCdi9hV0XENskQT1xaeQTIWIRv6S
-acMhaGuGFdVpNWvj9l8em+yag3+6xd5z43xFrjMKnS9l/zuIUn486nxR7Y9knShp
-iKQKgIM1Dnwcnr242F3uO0JwN6PItiDZtAXDAVUBAoGBAOPo/QSVGfZ1g9Lg9Xwf
-k2Psk2MMymXmRycWbaQJkYpTsVI+3nlhZz69WmqekBFKyjs32jAggbo8U9Zy0UMx
-J80BkIuZE10cAXiE2wEXoxbBPfXjMKwwf6h1Sm9XXVEHx01Cdtxl/llvTfEr7a9p
-Rg9v50EVbOTB+3s/Hc0RszTt
------END PRIVATE KEY-----
-`,
-    cert: `-----BEGIN CERTIFICATE-----
-MIICpDCCAYwCCQC+zR6wDbTx9zANBgkqhkiG9w0BAQsFADAUMRIwEAYDVQQDDAls
-b2NhbGhvc3QwHhcNMjMwMTAzMTkzODA1WhcNMjMwMjAyMTkzODA1WjAUMRIwEAYD
-VQQDDAlsb2NhbGhvc3QwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQDD
-mkdOoHdbvOshzJ87KpSu4I3KOPuO/2msLNYDmiyivGc79Y5T064sCxet0J/jfpU/
-7t9A4ETNGBi0/i4ZGa/kbR4uCQXnl2jroHuNFdfcnyBWjJLxaIN2WUm/RZvsjNSo
-WhkOjNjZZIsg9nGv6iJLNVH7RZf0bAmklo+Al6bXnsPY+9k8Apy74zniGBchJsU5
-q8Mg4eX8ctVpfaWLg0LUCOZGQZoJPc1ZsxcMGOBN+7CIONq/VeiNB8ILyadF6A4J
-ywr9FkJzCz0kr5uiXlYygsXSsMYTcScPy3Uqd3W9YKwhVaE8fsLa56+Gp08ifRny
-UC9E0y56BpOQSEGis/J5AgMBAAEwDQYJKoZIhvcNAQELBQADggEBAIE5EdZOzde9
-XqywKS3ms5FrBkajaR3K6XnyX6zAPK7/wBXvVmK7da6hMq5pYOQOIepdFs9FhX7l
-vu6KTA/rJ4gkRz08UPFIIbUtqHYmWCTWEv4BSDneXPCwIYHjM3TmcW6XtaS+i01u
-JKlZ0IwBc3rBSBMVf8Yo567W7JQebOe0LEOzGeYtFuAKPUSfTsPp2pyae/5ar5a6
-Hikm1olkZ74i7Sfg3/gr/VNsh30IVGvW7q3NSznvWOB0Dx6jdchw2kgUeNEUfoyE
-RMyjn6SZ8fxpOsqBOQLgaVgVeBTornOBI9BIV9sk/raT4ErG+H6GdCBhTK9QllSP
-J4aliShXnxA=
------END CERTIFICATE-----
-`,
-  };
+  const certLocalhost = getCertLocalhost();
 
   // The following servers are available through crosstests:
   //
@@ -916,4 +869,33 @@ J4aliShXnxA=
       }
     },
   };
+}
+
+let certLocalHost:
+  | {
+      key: string;
+      cert: string;
+    }
+  | undefined;
+
+function getCertLocalhost(): { key: string; cert: string } {
+  if (certLocalHost === undefined) {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    let dir = new URL(import.meta.url).pathname;
+    for (let i = 0; i < 10; i++) {
+      if (fs.existsSync(path.join(dir, "package.json"))) {
+        break;
+      }
+      dir = path.join(dir, "..");
+    }
+    const key = fs.readFileSync(path.join(dir, "localhost-key.pem"), {
+      encoding: "utf8",
+    });
+    const cert = fs.readFileSync(path.join(dir, "localhost-cert.pem"), {
+      encoding: "utf8",
+    });
+    certLocalHost = { key, cert };
+  }
+  return certLocalHost;
 }
