a2f7dccdca331037e61b807adcdf3c56a9297a3c
Timo Stamm <ts@timostamm.de>
2023-04-13 23:24:50 +0200

    Add case for 0H

--- packages/connect/src/protocol-grpc/parse-timeout.spec.ts
+++ packages/connect/src/protocol-grpc/parse-timeout.spec.ts
@@ -25,6 +25,7 @@ describe("parseTimeout()", function () {
     expect(parseTimeout("1000u")).toEqual(1);
     expect(parseTimeout("1000000n")).toEqual(1);
     expect(parseTimeout("0n")).toEqual(0);
+    expect(parseTimeout("0H")).toEqual(0);
   });
   it("should should return undefined for null value", () => {
     expect(parseTimeout(null)).toEqual(undefined);
