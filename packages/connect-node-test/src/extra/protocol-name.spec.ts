76d8f4150bbc2ff22b1a06dc448c30018419665b
John Chadwick <jchadwick@buf.build>
2023-05-11 09:50:10 -0400

    Add test for protocolName

---
+++ packages/connect-node-test/src/extra/protocol-name.spec.ts
@@ -0,0 +1,87 @@
+// Copyright 2021-2023 Buf Technologies, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import { createCallbackClient, createPromiseClient } from "@bufbuild/connect";
+import type { Transport } from "@bufbuild/connect";
+import { TestService } from "../gen/grpc/testing/test_connect.js";
+import { SimpleRequest } from "../gen/grpc/testing/messages_pb.js";
+import { createTestServers } from "../helpers/testserver.js";
+
+function ensureProtocolName(expectedName: string) {
+  return (header: Headers) => {
+    expect(header.get("request-protocol")).toEqual(expectedName);
+  };
+}
+
+function testForProtocol(expectedProtocolName: string) {
+  return (transportFactory: () => Transport) => {
+    const request = new SimpleRequest({
+      responseSize: 1024,
+      payload: {
+        body: new Uint8Array(1024).fill(0),
+      },
+    });
+    it("with promise client", async function () {
+      const transport = transportFactory();
+      const client = createPromiseClient(TestService, transport);
+      const response = await client.unaryCall(request, {
+        onHeader: ensureProtocolName(expectedProtocolName),
+      });
+      expect(response.payload).toBeDefined();
+      expect(response.payload?.body.length).toEqual(request.responseSize);
+    });
+    it("with callback client", function (done) {
+      const transport = transportFactory();
+      const client = createCallbackClient(TestService, transport);
+      client.unaryCall(
+        request,
+        (err, response) => {
+          expect(err).toBeUndefined();
+          expect(response.payload).toBeDefined();
+          expect(response.payload?.body.length).toEqual(request.responseSize);
+          done();
+        },
+        { onHeader: ensureProtocolName(expectedProtocolName) }
+      );
+    });
+  };
+}
+
+describe("protocolName", function () {
+  const servers = createTestServers();
+  beforeAll(async () => await servers.start());
+
+  servers.describeTransportsOnly(
+    [
+      "@bufbuild/connect-node (gRPC, binary, http2) against @bufbuild/connect-node (h2)",
+    ],
+    testForProtocol("grpc")
+  );
+
+  servers.describeTransportsOnly(
+    [
+      "@bufbuild/connect-node (gRPC-web, binary, http2) against @bufbuild/connect-node (h2c)",
+    ],
+    testForProtocol("grpc-web")
+  );
+
+  servers.describeTransportsOnly(
+    [
+      "@bufbuild/connect-node (Connect, binary, http2, gzip) against @bufbuild/connect-node (h2c)",
+    ],
+    testForProtocol("connect")
+  );
+
+  afterAll(async () => await servers.stop());
+});
