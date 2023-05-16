e585bcc380e35c237f044bc76ef2438f618bd671
Dimitri Mitropoulos <dimitrimitropoulos@gmail.com>
2023-05-16 15:49:57 -0400

    uses beforeEach

--- packages/connect-node/src/node-universal-handler.spec.ts
+++ packages/connect-node/src/node-universal-handler.spec.ts
@@ -110,36 +110,76 @@ describe("universalRequestFromNodeRequest()", function () {
     });
   });
 
-  const logEvents = (request: http.IncomingMessage | http.ClientRequest, message: string, skip?: boolean) => {
+  const logEvents = (
+    request: http.IncomingMessage | http.ClientRequest,
+    message: string,
+    skip?: boolean
+  ) => {
     console.log(`[HEY!] start: ${message}`);
 
     if (skip === false || skip === undefined) {
-      request.on('close', () => console.log(`[HEY!] close event with ${message}`));
-      request.on('data', (chunk) => console.log(`[HEY!] data event with ${message}`, chunk));
-      request.on('end', () => console.log(`[HEY!] end event with ${message}`));
-      request.on('error', (err) => console.log(`[HEY!] error event with ${message}`, err));
-      request.on('pause', () => console.log(`[HEY!] pause event with ${message}`));
-      request.on('readable', () => console.log(`[HEY!] readable event with ${message}`));
-      request.on('resume', () => console.log(`[HEY!] resume event with ${message}`));
+      request.on("close", () =>
+        console.log(`[HEY!] close event with ${message}`)
+      );
+      request.on("data", (chunk) =>
+        console.log(`[HEY!] data event with ${message}`, chunk)
+      );
+      request.on("end", () => console.log(`[HEY!] end event with ${message}`));
+      request.on("error", (err) =>
+        console.log(`[HEY!] error event with ${message}`, err)
+      );
+      request.on("pause", () =>
+        console.log(`[HEY!] pause event with ${message}`)
+      );
+      request.on("readable", () =>
+        console.log(`[HEY!] readable event with ${message}`)
+      );
+      request.on("resume", () =>
+        console.log(`[HEY!] resume event with ${message}`)
+      );
 
       if (request instanceof http.ClientRequest) {
-        request.on('abort', () => console.log(`[HEY!] abort event with ${message}`));
-        request.on('connect', (response, socket, head) => console.log(`[HEY!] connect event with ${message}`));
-        request.on('continue', () => console.log(`[HEY!] continue event with ${message}`));
-        request.on('drain', () => console.log(`[HEY!] drain event with ${message}`));
-        request.on('finish', () => console.log(`[HEY!] finish event with ${message}`));
-        request.on('information', (info) => console.log(`[HEY!] information event with ${message}`));
-        request.on('pipe', (src) => console.log(`[HEY!] pipe event with ${message}`));
-        request.on('response', (response) => console.log(`[HEY!] response event with ${message}`));
-        request.on('socket', (socket) => console.log(`[HEY!] socket event with ${message}`));
-        request.on('timeout', () => console.log(`[HEY!] timeout event with ${message}`));
-        request.on('unpipe', (src) => console.log(`[HEY!] unpipe event with ${message}`));
-        request.on('upgrade', (response, socket, head) => console.log(`[HEY!] upgrade event with ${message}`));
+        request.on("abort", () =>
+          console.log(`[HEY!] abort event with ${message}`)
+        );
+        request.on("connect", (response, socket, head) =>
+          console.log(`[HEY!] connect event with ${message}`)
+        );
+        request.on("continue", () =>
+          console.log(`[HEY!] continue event with ${message}`)
+        );
+        request.on("drain", () =>
+          console.log(`[HEY!] drain event with ${message}`)
+        );
+        request.on("finish", () =>
+          console.log(`[HEY!] finish event with ${message}`)
+        );
+        request.on("information", (info) =>
+          console.log(`[HEY!] information event with ${message}`)
+        );
+        request.on("pipe", (src) =>
+          console.log(`[HEY!] pipe event with ${message}`)
+        );
+        request.on("response", (response) =>
+          console.log(`[HEY!] response event with ${message}`)
+        );
+        request.on("socket", (socket) =>
+          console.log(`[HEY!] socket event with ${message}`)
+        );
+        request.on("timeout", () =>
+          console.log(`[HEY!] timeout event with ${message}`)
+        );
+        request.on("unpipe", (src) =>
+          console.log(`[HEY!] unpipe event with ${message}`)
+        );
+        request.on("upgrade", (response, socket, head) =>
+          console.log(`[HEY!] upgrade event with ${message}`)
+        );
       }
     }
 
     return () => console.log(`[HEY!] done: ${message}`);
-  }
+  };
 
   describe("with HTTP/1.1 ECONNRESET", function () {
     let serverAbortReason: undefined | unknown;
@@ -165,7 +205,11 @@ describe("universalRequestFromNodeRequest()", function () {
         const request = http.request(server.getUrl(), {
           method: "POST",
         });
-        const done = logEvents(request, "should abort request signal with ConnectError and Code.Aborted", true);
+        const done = logEvents(
+          request,
+          "should abort request signal with ConnectError and Code.Aborted",
+          true
+        );
         request.on("error", () => {
           // we need this event lister so that Node.js does not raise the error
           // we trigger by calling destroy()
@@ -191,31 +235,44 @@ describe("universalRequestFromNodeRequest()", function () {
 
   describe("with HTTP/1.1 request finishing without error", function () {
     let universalRequestSignal: AbortSignal | undefined;
-    const server = () => useNodeServer(() =>
-      http.createServer(
-        {
-          connectionsCheckingInterval: 1,
-        },
-        function (request, response) {
-          const done = logEvents(request, "HTTP/1.1 request finishing server handler");
-          const uReq = universalRequestFromNodeRequest(request, undefined);
-          universalRequestSignal = uReq.signal;
-          response.writeHead(200);
-          response.end();
-          done()
+    let server:
+      | {
+          getUrl(): string;
         }
-      )
-    );
+      | undefined;
+    beforeEach(() => {
+      server = useNodeServer(() =>
+        http.createServer(
+          {
+            connectionsCheckingInterval: 1,
+          },
+          function (request, response) {
+            const done = logEvents(
+              request,
+              "HTTP/1.1 request finishing server handler"
+            );
+            const uReq = universalRequestFromNodeRequest(request, undefined);
+            universalRequestSignal = uReq.signal;
+            response.writeHead(200);
+            response.end();
+            done();
+          }
+        )
+      );
+    });
 
     // this one too
     it("should abort request signal with AbortError", async function () {
       await new Promise<void>((resolve) => {
-        const request = http.request(server().getUrl(), {
+        const request = http.request(server!.getUrl(), {
           method: "POST",
           // close TCP connection after we're done so that the server shuts down cleanly
           agent: new http.Agent({ keepAlive: false }),
         });
-        const done = logEvents(request, "with HTTP/1.1 request finishing client starting request")
+        const done = logEvents(
+          request,
+          "with HTTP/1.1 request finishing client starting request"
+        );
         request.flushHeaders();
         request.end();
         request.on("response", (response) => {
