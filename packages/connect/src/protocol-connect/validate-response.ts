bb9dbc83ddf4f2eb49e441eda072d87e2b15a0c1
Steve Ayers <sayers@buf.build>
2023-06-27 14:20:54 -0400

    Do not parse as JSON

--- packages/connect/src/protocol-connect/validate-response.ts
+++ packages/connect/src/protocol-connect/validate-response.ts
@@ -16,6 +16,7 @@ import { MethodKind } from "@bufbuild/protobuf";
 import { Code } from "../code.js";
 import { codeFromHttpStatus } from "./http-status.js";
 import { ConnectError } from "../connect-error.js";
+import { parseContentType } from "../content-type.js";
 import { headerStreamEncoding, headerUnaryEncoding } from "./headers.js";
 import type { Compression } from "../protocol/compression.js";
 
@@ -36,13 +37,15 @@ export function validateResponse(
 ):
   | { isUnaryError: false; unaryError?: undefined }
   | { isUnaryError: true; unaryError: ConnectError } {
+  const mimeType = headers.get("Content-Type");
+  const parsedType = parseContentType(mimeType);
   if (status !== 200) {
     const errorFromStatus = new ConnectError(
       `HTTP ${status}`,
       codeFromHttpStatus(status),
       headers
     );
-    if (methodKind == MethodKind.Unary) {
+    if (methodKind == MethodKind.Unary && parsedType && !parsedType.stream) {
       return { isUnaryError: true, unaryError: errorFromStatus };
     }
     throw errorFromStatus;
