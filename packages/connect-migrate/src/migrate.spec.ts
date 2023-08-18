be1e688fa027822d39433d84f4f124e5854f385e
Timo Stamm <ts@timostamm.de>
2023-08-18 16:51:18 +0200

    Improve DX with migration CLI (#776)

--- packages/connect-migrate/src/migrate.spec.ts
+++ packages/connect-migrate/src/migrate.spec.ts
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { replacePackageJSONReferences } from "./replacement-map";
+import { replacePackageJSONReferences } from "./migrate";
 
 describe("replacePackageJSONReferences", () => {
   it("handles parsing package.json", () => {
