388a7d904c082f806bafe06b4b2358c2026d4f8d
Steve Ayers <sayers@buf.build>
2024-05-07 17:52:59 -0400

    Remove unused

--- packages/connect-conformance/src/index.ts
+++
@@ -1,9 +0,0 @@
-export { default as routes } from "./routes.js";
-export { run } from "./conformance.js";
-
-export * from "./gen/connectrpc/conformance/v1/client_compat_pb.js";
-export * from "./gen/connectrpc/conformance/v1/config_pb.js";
-export * from "./gen/connectrpc/conformance/v1/server_compat_pb.js";
-export * from "./gen/connectrpc/conformance/v1/service_connect.js";
-export * from "./gen/connectrpc/conformance/v1/service_pb.js";
-export * from "./gen/connectrpc/conformance/v1/suite_pb.js";
