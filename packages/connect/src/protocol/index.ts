bfaf9875f1d17075d386455b0cc2123b19084a27
Cheng Wang <wangcheng678@gmail.com>
2024-12-03 12:12:36 +0800

    export functions in normalize.ts

--- packages/connect/src/protocol/index.ts
+++ packages/connect/src/protocol/index.ts
@@ -114,3 +114,4 @@ export {
 export type { UniversalHandlerOptions } from "./universal-handler.js";
 export type { ProtocolHandlerFactory } from "./protocol-handler-factory.js";
 export type { CommonTransportOptions } from "./transport-options.js";
+export { normalize, normalizeIterable } from "./normalize.js";
